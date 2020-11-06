9a62333c6d234ed6f856d96b0ef019c6a6ce0568
Aishwarya <amuthyala511@gmail.com>
2020-11-06 09:19:43 +0530

    searching for particular node in the Binary Search Tree.

--- BinarySearchTreeUsingGenerics/src/main/java/com/blz/bst/generics/MyBinaryTree.java
+++ BinarySearchTreeUsingGenerics/src/main/java/com/blz/bst/generics/MyBinaryTree.java
@@ -24,4 +24,16 @@ public class MyBinaryTree<K extends Comparable<K>> {
 	 private int getSizeRecursively(MyBinaryNode<K> nodeValue) {
 		 return nodeValue == null ? 0 : 1 + this.getSizeRecursively(nodeValue.leftchild)+this.getSizeRecursively(nodeValue.rightchild);
 	 }
+	 
+	 public boolean searchNode(MyBinaryNode<K> rootNode, int node) {
+		 if(rootNode == null)
+			 return false;
+		 else if((int)rootNode.key == node)
+			 return true;
+		 else if(node < (int)rootNode.key)
+			 return searchNode(rootNode.leftchild, node);
+		 else if(node > (int)rootNode.key)
+			 return searchNode(rootNode.rightchild, node);
+		return false;
+	 }
 }
--- BinarySearchTreeUsingGenerics/src/test/java/com/blz/bst/generics/MyBinaryTest.java
+++ BinarySearchTreeUsingGenerics/src/test/java/com/blz/bst/generics/MyBinaryTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyBinaryTest {
+	
+	//UC1
 	@Test
 	public void added3Numbers_IntoBST() {
 		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
@@ -14,6 +16,7 @@ public class MyBinaryTest {
 		Assert.assertEquals(3, size);
 	}
 	
+	//UC2
 	@Test
 	public void addingNumbers_moreThan3_ToBST() {
 		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
@@ -32,5 +35,26 @@ public class MyBinaryTest {
 		bst.add(3);
 		int size = bst.getSize();
 		Assert.assertEquals(13, size);
+	}
+	
+	//UC3
+	@Test
+	public void searchForNode_InTheBST() {
+		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
+		bst.add(56);
+		bst.add(30);
+		bst.add(70);
+		bst.add(60);
+		bst.add(95);
+		bst.add(65);
+		bst.add(63);
+		bst.add(67);
+		bst.add(40);
+		bst.add(22);
+		bst.add(11);
+		bst.add(16);
+		bst.add(3);
+		boolean result = bst.searchNode(bst.rootNode, 63);
+		Assert.assertEquals(true, result);
 	}
 }
